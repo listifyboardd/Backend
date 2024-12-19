0329781fac895b51409bcee45724e5403541bcb7
vitalikkutnyi <vitalikkutnyi0@gmail.com>
2024-12-19 18:51:39 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -169,7 +169,7 @@ REST_FRAMEWORK = {
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 15,
+    'PAGE_SIZE': 14,
 }
 
 
